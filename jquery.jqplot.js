6725bb82db93eba45b6d792212e78f3a4c1aa6b0
Chris Leonello <chris.leonello@gmail.com>
2009-03-25 02:06:38 +0000

    Removed makeTag function, just use jQuery!
    --HG--
    extra : convert_revision : svn%3A4a7f7d83-b634-4dba-bb5d-c55b4c2ed08c/trunk%4010

--- jquery.jqplot.js
+++ jquery.jqplot.js
@@ -131,6 +131,9 @@
         this.equalYTicks = true;
         // borrowed colors from Flot.
         this.seriesColors = ["#edc240", "#afd8f8", "#cb4b4b", "#4da74d", "#9440ed"];
+        // hooks for plugins
+        this.preDrawHooks = [];
+        this.postDrawHooks = [];
             
         this.init = function(target, data, options) {
             this.targetId = target;
@@ -274,12 +277,18 @@
     
         // create the plot and add it do the dom
         this.draw = function(){
+            for (var i=0; i<this.preDrawHooks.length; i++) {
+                this.preDrawHooks[i].call(this);
+            }
             this.drawTitle();
             this.drawAxes();
             this.pack();
             this.makeCanvas();
             this.drawGrid();
             this.drawSeries();
+            for (var i=0; i<this.postDrawHooks.length; i++) {
+                this.postDrawHooks[i].call(this);
+            }
         };
     
         // Add the canvas element to the DOM
@@ -297,16 +306,22 @@
                     // Need to pregenerate each axis to get it's bounds and
                     // position it and the labels correctly on the plot.
                     var h, w;
-                    axis.elem = makeTag('div', {class:'jqplot', style:axis.style});
-                    // divs don't have dimensions untill added to dom
-                    this.target.append(axis.elem);
+                    
+                    axis.elem = $('<div class="jqplot-axis">').appendTo(this.target).get(0);
+                    for (var s in axis.style) $(axis.elem).css(s, axis.style[s]);
+
                     for (var i=0; i<axis.ticks.labels.length; i++) {
-                        var elem = makeTag('div', {class:'jqplot', style:axis.ticks.styles[i]}, axis.ticks.labels[i]);
-                        axis.elem.appendChild(elem);
+                        var elem = $('<div class="jqplot-axis-tick">').appendTo(axis.elem).get(0);
+                        
+                        for (var s in axis.ticks.styles[i]) $(elem).css(s, axis.ticks.styles[i][s]);
+                        $(elem).html(axis.ticks.labels[i]);
+                        
                         if (axis.ticks.fontFamily) elem.style.fontFamily = axis.ticks.fontFamily;
                         if (axis.ticks.fontSize) elem.style.fontSize = axis.ticks.fontSize;
+                        
                         h = $(elem).outerHeight();
                         w = $(elem).outerWidth();
+                        
                         if (axis.height < h) {
                             axis.height = h;
                             this.gridOffsets[axis.gridOffset] = h;
@@ -668,70 +683,6 @@
             yellow:[255,255,0]
         }; 
 
-    /**
-     * From "JavaScript: the Definitive Guide, by David Flanagan. Copyright 2006 O'Reilly Media, Inc."
-     * 
-     * make(tagname, attributes, children):
-     *   create an HTML element with specified tagname, attributes and children.
-     * 
-     * The attributes argument is a JavaScript object: the names and values of its
-     * properties are taken as the names and values of the attributes to set.
-     * If attributes is null, and children is an array or a string, the attributes 
-     * can be omitted altogether and the children passed as the second argument. 
-     *
-     * The children argument is normally an array of children to be added to 
-     * the created element.  If there are no children, this argument can be 
-     * omitted.  If there is only a single child, it can be passed directly 
-     * instead of being enclosed in an array. (But if the child is not a string
-     * and no attributes are specified, an array must be used.)
-     * 
-     * Example: make("p", ["This is a ", make("b", "bold"), " word."]);
-     *
-     * Inspired by the MochiKit library (http://mochikit.com) by Bob Ippolito
-     */
-    function makeTag(tagname, attributes, children) {
-
-        // If we were invoked with two arguments the attributes argument is
-        // an array or string, it should really be the children arguments.
-        if (arguments.length == 2 && 
-            (attributes instanceof Array || typeof attributes == "string")) {
-            children = attributes;
-            attributes = null;
-        }
-
-        // Create the element
-        var e = document.createElement(tagname);
-
-        // Set attributes
-        if (attributes) {
-            for(var name in attributes) {
-                if (name == 'style') {    // handle a style object
-                    for (var s in attributes[name]) {
-                        e.style[s] = attributes[name][s].toString();
-                    }
-                }
-                else e.setAttribute(name, attributes[name]);
-            }
-        }
-
-        // Add children, if any were specified.
-        if (children != null) {
-            if (children instanceof Array) {  // If it really is an array
-                for(var i = 0; i < children.length; i++) { // Loop through kids
-                    var child = children[i];
-                    if (typeof child == "string")          // Handle text nodes
-                        child = document.createTextNode(child);
-                    e.appendChild(child);  // Assume anything else is a Node
-                }
-            }
-            else if (typeof children == "string") // Handle single text child
-                e.appendChild(document.createTextNode(children));
-            else e.appendChild(children);         // Handle any other single child
-        }
-
-        // Finally, return the element.
-        return e;
-    };
 	
 	// Convienence function that won't hang IE.
 	function log(something) {
