8c7585b5e2677eace5c3073df31f1df2e675ef69
Chris Leonello <chris.leonello@gmail.com>
2009-04-20 01:52:13 +0000

    Have the source bootloader working.
    --HG--
    extra : convert_revision : svn%3A4a7f7d83-b634-4dba-bb5d-c55b4c2ed08c/trunk%4061

---
+++ src/jquery.jqplot.js
@@ -0,0 +1,38 @@
+// This is a bootstrap loader for using the source distribution of jqPlot.
+// Unless you are doing development, you probably want one of the prepackaged distributions.
+// You could also load all of the source files separately.
+(function(){
+	var getRootNode = function(){
+		// figure out the path to this loader
+		if(this["document"] && this["document"]["getElementsByTagName"]){
+			var scripts = document.getElementsByTagName("script");
+			var pat = /jquery\.jqplot\.js/i;
+			for(var i = 0; i < scripts.length; i++){
+				var src = scripts[i].getAttribute("src");
+				if(!src){ continue; }
+				var m = src.match(pat);
+				if(m){
+					return { 
+						node: scripts[i], 
+						root: src.substring(0, m.index)
+					};
+				}
+			}
+		}
+	}
+
+
+    var files = ['jqplot.core.js', 'jqplot.linearAxisRenderer.js', 'jqplot.axisTickRenderer.js', 'jqplot.tableLegendRenderer.js', 'jqplot.lineRenderer.js', 'jqplot.markerRenderer.js', 'jqplot.divTitleRenderer.js', 'jqplot.canvasGridRenderer.js', 'jqplot.sprintf.js'];
+    var rn = getRootNode().root;
+    for (var i=0; i<files.length; i++) {
+        var pp = rn+files[i];
+        try {
+            document.write("<scr"+"ipt type='text/javascript' src='"+pp+"'></scr"+"ipt>");
+        } catch (e) {
+            var script = document.createElement("script");
+            script.src = pp;
+            document.getElementsByTagName("head")[0].appendChild(script);
+        }
+    }
+    
+})();
