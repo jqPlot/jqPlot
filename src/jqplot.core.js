7f448028fe8b3325942a5469f27d735cae9f2584
Chris Leonello <chris@jqplot.com>
2009-06-01 03:15:01 -0400

    Some clarification in documentation.

--- src/jqplot.core.js
+++ src/jqplot.core.js
@@ -15,10 +15,21 @@
  * 
  * About: Introduction
  * 
+ * jqPlot requires jQuery (tested with 1.3.2 or better). jQuery 1.3.2 is included in the distribution.  
+ * To use jqPlot include jQuery, the jqPlot jQuery plugin, and optionally the excanvas script for IE support in your web page:
+ * 
+ * > <!--[if IE]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
+ * > <script language="javascript" type="text/javascript" src="jquery-1.3.2.min.js"></script>
+ * > <script language="javascript" type="text/javascript" src="jquery.jqplot.min.js"></script>
+ * 
  * jqPlot can be customized by overriding the defaults of any of the objects which make
  * up the plot.  The general usage of jqplot is:
  * 
  * > chart = $.jqplot('targetElemId', [dataArray,...], {optionsObject});
+ * 
+ * Which would look like this in actual code:
+ * 
+ * > $.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
  * 
  * The optionsObject corresponds to an properties on a <jqPlot> instance, so customization
  * may look like this:
