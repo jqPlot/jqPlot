e62bc8d80d1189b37245be8967702c52c5203796
Chris Leonello <chris@jqplot.com>
2009-06-01 22:26:24 -0400

    Fixed up some styling and display of documentation. Removed unused samples folder. renamed used samples folder to images.
    --HG--
    rename : src/samples/background.jpg => src/images/background.jpg
    rename : src/samples/basicline.png => src/images/basicline.png
    rename : src/samples/basiclogaxis.png => src/images/basiclogaxis.png
    rename : src/samples/basiclogoptions.png => src/images/basiclogoptions.png
    rename : src/samples/basicoptions.png => src/images/basicoptions.png
    rename : src/samples/dualaxis.png => src/images/dualaxis.png
    rename : src/samples/logo.jpg => src/images/logo.jpg
    rename : src/samples/navdocs.png => src/images/navdocs.png
    rename : src/samples/navdocsover.png => src/images/navdocsover.png
    rename : src/samples/navdownload.png => src/images/navdownload.png
    rename : src/samples/navdownloadover.png => src/images/navdownloadover.png
    rename : src/samples/navexamples.png => src/images/navexamples.png
    rename : src/samples/navexamplesover.png => src/images/navexamplesover.png
    rename : src/samples/navhome.gif => src/images/navhome.gif
    rename : src/samples/navhome.png => src/images/navhome.png
    rename : src/samples/navhomeover.png => src/images/navhomeover.png
    rename : src/samples/sample3.png => src/images/sample3.png
    rename : src/samples/samplesm.png => src/images/samplesm.png

--- src/jqplot.core.js
+++ src/jqplot.core.js
@@ -1,7 +1,7 @@
 /**
- * Title: jqPlot
+ * Title: jqPlot Charts
  * 
- * Pure JavaScript plotting library for jQuery.
+ * Pure JavaScript plotting plugin for jQuery.
  * 
  * About: Version
  * 
@@ -27,7 +27,8 @@
  * 
  * > chart = $.jqplot('targetElemId', [dataArray,...], {optionsObject});
  * 
- * Which would look like this in actual code:
+ * Which would look like this 
+ * in actual code:
  * 
  * > $.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
  * 
