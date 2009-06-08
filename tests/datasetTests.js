bfd083ddccf25af981cf38d2df9551111a2d35ee
Chris Leonello <chris@jqplot.com>
2009-06-08 10:44:32 -0400

    Improperly configured test spec.

--- tests/datasetTests.js
+++ tests/datasetTests.js
@@ -6,15 +6,21 @@ describe 'Equal Axis Coordinate Values'
         plot1.series.should.have_property 'length', 1
     end
     it plot2.title.text            
-        plot1.axes.xaxis.should.have_property '_elem'
-        plot1.axes.yaxis.should.have_property '_elem'
-        plot1.seriesCanvas.should.have_property '_elem'
-        plot1.series.should.have_property 'length', 1
+        plot2.axes.xaxis.should.have_property '_elem'
+        plot2.axes.yaxis.should.have_property '_elem'
+        plot2.seriesCanvas.should.have_property '_elem'
+        plot2.series.should.have_property 'length', 1
     end
     it plot3.title.text            
-        plot1.axes.xaxis.should.have_property '_elem'
-        plot1.axes.yaxis.should.have_property '_elem'
-        plot1.seriesCanvas.should.have_property '_elem'
-        plot1.series.should.have_property 'length', 1
+        plot3.axes.xaxis.should.have_property '_elem'
+        plot3.axes.yaxis.should.have_property '_elem'
+        plot3.seriesCanvas.should.have_property '_elem'
+        plot3.series.should.have_property 'length', 1
+    end
+    it plot4.title.text            
+        plot4.axes.xaxis.should.have_property '_elem'
+        plot4.axes.yaxis.should.have_property '_elem'
+        plot4.seriesCanvas.should.have_property '_elem'
+        plot4.series.should.have_property 'length', 1
     end
 end
