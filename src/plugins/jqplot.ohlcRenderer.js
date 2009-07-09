2ef0888a144af3782fc1a2a0132566658511fb1c
Chris Leonello <chris@jqplot.com>
2009-07-09 16:31:55 -0400

    Added ability to handle hlc charts.  Auto detect if data is ohlc or hlc. Added default sizing of candle body and tick length based on number of points displayed.  Helps for zooming.

--- src/plugins/jqplot.ohlcRenderer.js
+++ src/plugins/jqplot.ohlcRenderer.js
@@ -9,7 +9,8 @@
         // subclass line renderer to make use of some of it's methods.
         $.jqplot.LineRenderer.call(this);
         // prop: candleStick
-        // true to render chart as candleStick
+        // true to render chart as candleStick.
+        // Must have an open price, cannot be a hlc chart.
         this.candleStick = false;
         this.tickLength = null;
         this.bodyWidth = null;
@@ -20,7 +21,9 @@
         this.fillDownBody = true;
         this.upBodyColor = null;
         this.downBodyColor = null;
-        
+        // prop: hlc
+        // true if is a hi-low-close chart (no open price).
+        this.hlc = false;
         this.candleWidth
     };
     
@@ -34,14 +37,28 @@
         // set the yaxis data bounds here to account for hi and low values
         var db = this._yaxis._dataBounds;
         var d = this._plotData;
+        // if data points have less than 5 values, force a hlc chart.
+        if (d[0].length < 5) {
+            this.renderer.hlc = true;
 
-        for (var j=0; j<d.length; j++) { 
-            if (d[j][3] < db.min || db.min == null) {
-            	db.min = d[j][3];
+            for (var j=0; j<d.length; j++) { 
+                if (d[j][2] < db.min || db.min == null) {
+                	db.min = d[j][2];
+                }
+                if (d[j][1] > db.max || db.max == null) {
+                	db.max = d[j][1];
+                }             
             }
-            if (d[j][2] > db.max || db.max == null) {
-            	db.max = d[j][2];
-            }             
+        }
+        else {
+            for (var j=0; j<d.length; j++) { 
+                if (d[j][3] < db.min || db.min == null) {
+                	db.min = d[j][3];
+                }
+                if (d[j][2] > db.max || db.max == null) {
+                	db.max = d[j][2];
+                }             
+            }
         }
         
     };
@@ -49,6 +66,12 @@
     // called within scope of series.
     $.jqplot.OHLCRenderer.prototype.draw = function(ctx, gd, options) {
         var d = this.data;
+        var xmin = this._xaxis.min;
+        var xmax = this._xaxis.max;
+        // index of last value below range of plot.
+        var xminidx = 0;
+        // index of first value above range of plot.
+        var xmaxidx = d.length;
         var xp = this._xaxis.series_u2p;
         var yp = this._yaxis.series_u2p;
         var i, prevColor, ops, b, h, w, a, points;
@@ -58,24 +81,45 @@
         var shadow = (opts.shadow != undefined) ? opts.shadow : this.shadow;
         var fill = (opts.fill != undefined) ? opts.fill : this.fill;
         var fillAndStroke = (opts.fillAndStroke != undefined) ? opts.fillAndStroke : this.fillAndStroke;
+        var bodyWidth = (opts.bodyWidth != undefined) ? opts.bodyWidth : this.bodyWidth;
+        var tickLength = (opts.tickLength != undefined) ? opts.tickLength : this.tickLength;
         ctx.save();
         if (this.show) {
             var x, open, hi, low, close;
-            if (r.candleStick && r.bodyWidth == null) {
-                r.bodyWidth = ctx.canvas.width/d.length/2;
-            }
-            else if (!r.candleStick && r.tickLength == null) {
-                r.tickLength = ctx.canvas.width/d.length/4;
-            }
+            // need to get widths based on number of points shown,
+            // not on total number of points.  Use the results 
+            // to speed up drawing in next step.
             for (var i=0; i<d.length; i++) {
+                if (d[i][0] < xmin) {
+                    xminidx = i;
+                }
+                else if (d[i][0] < xmax) {
+                    xmaxidx = i+1;
+                }
+            }
+            if (r.candleStick && bodyWidth == null) {
+                bodyWidth = Math.min(20, ctx.canvas.width/(xmaxidx - xminidx)/2);
+            }
+            else if (!r.candleStick && tickLength == null) {
+                tickLength = Math.min(10, ctx.canvas.width/(xmaxidx - xminidx)/4);
+            }
+            for (var i=xminidx; i<xmaxidx; i++) {
                 x = xp(d[i][0]);
-                open = yp(d[i][1]);
-                hi = yp(d[i][2]);
-                low = yp(d[i][3]);
-                close = yp(d[i][4]);
+                if (r.hlc) {
+                    open = null;
+                    hi = yp(d[i][1]);
+                    low = yp(d[i][2]);
+                    close = yp(d[i][3]);
+                }
+                else {
+                    open = yp(d[i][1]);
+                    hi = yp(d[i][2]);
+                    low = yp(d[i][3]);
+                    close = yp(d[i][4]);
+                }
                 o = {};
-                if (r.candleStick) {
-                    w = r.bodyWidth;
+                if (r.candleStick && !r.hlc) {
+                    w = bodyWidth;
                     a = x - w/2;
                     // draw candle
                     // determine if candle up or down
@@ -158,19 +202,21 @@
                         opts.color = r.openColor;
                     }
                     // draw open tick
-                    r.shapeRenderer.draw(ctx, [[x-r.tickLength, yp(d[i][1])], [x, yp(d[i][1])]], opts); 
+                    if (!r.hlc) {
+                        r.shapeRenderer.draw(ctx, [[x-tickLength, open], [x, open]], opts);    
+                    }
                     opts.color = prevColor;
                     // draw wick
                     if (r.wickColor) {
                         opts.color = r.wickColor;
                     }
-                    r.shapeRenderer.draw(ctx, [[x, yp(d[i][2])], [x, yp(d[i][3])]], opts); 
+                    r.shapeRenderer.draw(ctx, [[x, hi], [x, low]], opts); 
                     opts.color  = prevColor;
                     // draw close tick
                     if (r.closeColor) {
                         opts.color = r.closeColor;
                     }
-                    r.shapeRenderer.draw(ctx, [[x, yp(d[i][4])], [x+r.tickLength, yp(d[i][4])]], opts); 
+                    r.shapeRenderer.draw(ctx, [[x, close], [x+tickLength, close]], opts); 
                     opts.color = prevColor;
                 }
             }
@@ -179,8 +225,24 @@
         ctx.restore();
     };  
     
-    $.jqplot.LineRenderer.prototype.drawShadow = function(ctx, gd, options) {
+    $.jqplot.OHLCRenderer.prototype.drawShadow = function(ctx, gd, options) {
         // This is a no-op, shadows drawn with lines.
     };
+    
+    // called with scope of plot.
+    $.jqplot.OHLCRenderer.checkOptions = function(target, data, options) {
+        // provide some sensible highlighter options by default
+        // These aren't good for hlc, only for ohlc or candlestick
+        if (!options.highlighter) {
+            options.highlighter = {
+                showMarker:false,
+                tooltipAxes: 'y',
+                yvalues: 4,
+                formatString:'<table class="jqplot-highlighter"><tr><td>date:</td><td>%s</td></tr><tr><td>open:</td><td>%s</td></tr><tr><td>hi:</td><td>%s</td></tr><tr><td>low:</td><td>%s</td></tr><tr><td>close:</td><td>%s</td></tr></table>'
+            };
+        }
+    }
+    
+	//$.jqplot.preInitHooks.push($.jqplot.OHLCRenderer.checkOptions);
     
 })(jQuery);    
