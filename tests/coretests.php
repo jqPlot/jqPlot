68326a5d656cdbba16b3810c63d045eb566127a9
Chris Leonello <chris@jqplot.com>
2009-07-15 12:49:56 -0400

    Updated version and change log in preparation for 0.9.1 release. Minor mods to docs and tests. Forgot to add link on tests index page.

--- tests/coretests.php
+++ tests/coretests.php
@@ -9,7 +9,7 @@
 <p class="description">jqPlot has numerous options for styling.  Out of the box, it supports lines with customizable widths, markers (circles, squares, diamonds either filled or open), and colors.</p>
 <p class="description">The plot title is specified by the "title" attribute.  Options for the title can be used by creating a "title" object instead of a "title" string.'</p>
 
-<div class="jqPlot" id="chart1" style="height:380px; width:540px;"></div>
+<div class="jqPlot" id="chart1" style="height:320px; width:540px;"></div>
      
 <pre class="prettyprint plot">
 var cosPoints = [];
@@ -42,7 +42,7 @@ plot1 = $.jqplot('chart1', [cosPoints, sinPoints, powPoints1, powPoints2], {
       
 <p class="description">Shadows are also customizable.  The angle, offset (distance from the line), alpha (transparency or darkness) and depth (number of shadow strokes, offset by "shadowOffset" from eachother) are all customizable.</p>
 
-<div class="jqPlot" id="chart2" style="height:380px; width:540px;"></div>
+<div class="jqPlot" id="chart2" style="height:360px; width:540px;"></div>
      
 <pre class="prettyprint plot"> 
 var cosPoints = []; 
@@ -67,7 +67,7 @@ plot2 = $.jqplot('chart2', [cosPoints], {
 <p class="description">Line data can be specified as an array of [x, y] pairs, or as an simple 1-D array.  If a 1-D array is given, x values are automatically generated starting at one and increasing by one for each y value in the array.</p>
 <p class="description">Line labels are specified in the "series" options.  The "series" option is an array of option objects, one for each series.  Here, the first series ("Rising line") is rendered as only markers by setting "showLine:false".</p>
 
-<div class="jqPlot" id="chart3" style="height:380px; width:540px;"></div>
+<div class="jqPlot" id="chart3" style="height:360px; width:540px;"></div>
      
 <pre class="prettyprint plot"> 
 line1=[[1,1], [1.5, 2.25], [2,4], [2.5,6.25], [3,9], [3.5,12.25], [4,16]];
@@ -87,7 +87,7 @@ plot3 = $.jqplot('chart3', [line1, line2, line3], {
 <p class="description">Axes ticks are computed automatically from the data, but can be customized if desired.  Ticks can be specified as an array of [value, label] pairs, or as a 1-D array of labels.</p>
 <p class="description">Here the y axis ticks use a custom format string ("%d") to produce integer tick labels.  The default format string is %.1f and can be customized to any sprintf style format string.Additionally, the grid background color and grid line color have been changed.</p>
 
-<div class="jqPlot" id="chart4" style="height:380px; width:540px;"></div>
+<div class="jqPlot" id="chart4" style="height:320px; width:540px;"></div>
      
 <pre class="prettyprint plot">
 line1=[[1,1], [1.5, 2.25], [2,4], [2.5,6.25], [3,9], [3.5,12.25], [4,16]];
