7ed8556c4a58b17e0c40e818f5d8ad75bacf4ee2
Chris Leonello <chris@jqplot.com>
2010-11-17 14:08:55 -0500

    This fixes #275 where IE 6/7 do not support array style indexing of strings.

--- src/jqplot.core.js
+++ src/jqplot.core.js
@@ -2940,7 +2940,7 @@
     $.jqplot.hex2rgb = function(h, a) {
         h = h.replace('#', '');
         if (h.length == 3) {
-            h = h[0]+h[0]+h[1]+h[1]+h[2]+h[2];
+            h = h.charAt(0)+h.charAt(0)+h.charAt(1)+h.charAt(1)+h.charAt(2)+h.charAt(2);
         }
         var rgb;
         rgb = 'rgba('+parseInt(h.slice(0,2), 16)+', '+parseInt(h.slice(2,4), 16)+', '+parseInt(h.slice(4,6), 16);
