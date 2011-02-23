3f6a8826f28c6edab73b3ae9ae25cf598505ec58
Chris Leonello <chris@jqplot.com>
2011-02-23 00:24:21 -0500

    fixed bug where databounds nulled out prematurely.

--- src/plugins/jqplot.dateAxisRenderer.js
+++ src/plugins/jqplot.dateAxisRenderer.js
@@ -144,10 +144,10 @@
                     d[j][0] = new $.jsDate(d[j][0]).getTime();
                     pd[j][0] = new $.jsDate(d[j][0]).getTime();
                     sd[j][0] = new $.jsDate(d[j][0]).getTime();
-                    if (d[j][0] < db.min || db.min == null) {
+                    if (db.min == null || d[j][1] < db.min) {
                         db.min = d[j][0];
                     }
-                    if (d[j][0] > db.max || db.max == null) {
+                    if (db.max == null || d[j][1] > db.max) {
                         db.max = d[j][0];
                     }
 					if (j>0) {
@@ -167,10 +167,10 @@
                     d[j][1] = new $.jsDate(d[j][1]).getTime();
                     pd[j][1] = new $.jsDate(d[j][1]).getTime();
                     sd[j][1] = new $.jsDate(d[j][1]).getTime();
-                    if (d[j][1] < db.min || db.min == null) {
+                    if (db.min == null || d[j][1] < db.min) {
                         db.min = d[j][1];
                     }
-                    if (d[j][1] > db.max || db.max == null) {
+                    if (db.max == null || d[j][1] > db.max) {
                         db.max = d[j][1];
                     }
 					if (j>0) {
@@ -199,8 +199,9 @@
 			stats.max = $.jqplot.arrayMax(stats.intervals);
 			stats.mean = sum/d.length;
 			this._intervalStats.push(stats);
-			db = stats = sum = s = d = pd = sd = null;
+			stats = sum = s = d = pd = sd = null;
         }
+		db = null;
 		
     };
     
