df6ff6666d99beb4eb652f8d27248829c7ccd7e4
Chris Leonello <chris@jqplot.com>
2011-08-22 00:09:11 -0400

    Updating examples to include view as image / save as image buttons.

--- examples/example.js
+++ examples/example.js
@@ -9,4 +9,25 @@ $(document).ready(function(){
         }
     });
     $(document).unload(function() {$('*').unbind(); });
+
+    if (!$.jqplot.use_excanvas) {
+        $('div.jqplot-target').each(function(){
+            // Add a view image button
+            var btn = $(document.createElement('button'));
+            btn.text('View as PNG');
+            btn.bind('click', {chart: $(this)}, function(evt) {
+            evt.data.chart.jqplotViewImage();
+            });
+            $(this).after(btn);
+
+            // add a save image button
+            btn = $(document.createElement('button'));
+            btn.text('Save as PNG');
+            btn.bind('click', {chart: $(this)}, function(evt) {
+            evt.data.chart.jqplotSaveImage();
+            });
+            $(this).after(btn);
+            btn = null;
+        });
+    }
 });
