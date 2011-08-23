70dd4d0803ddcdc47dc51dbaf3735fd14d49741c
Chris Leonello <chris@jqplot.com>
2011-08-23 08:14:21 -0400

    Updating copyright.  Adding wrapped text support to image export.

--- src/jqplot.toImage.js
+++ src/jqplot.toImage.js
@@ -1,3 +1,62 @@
+/**
+ * jqPlot
+ * Pure JavaScript plotting plugin using jQuery
+ *
+ * Version: @VERSION
+ *
+ * Copyright (c) 2009-2011 Chris Leonello
+ * jqPlot is currently available for use in all personal or commercial projects 
+ * under both the MIT (http://www.opensource.org/licenses/mit-license.php) and GPL 
+ * version 2.0 (http://www.gnu.org/licenses/gpl-2.0.html) licenses. This means that you can 
+ * choose the license that best suits your project and use it accordingly. 
+ *
+ * Although not required, the author would appreciate an email letting him 
+ * know of any substantial use of jqPlot.  You can reach the author at: 
+ * chris at jqplot dot com or see http://www.jqplot.com/info.php .
+ *
+ * If you are feeling kind and generous, consider supporting the project by
+ * making a donation at: http://www.jqplot.com/donate.php .
+ *
+ * sprintf functions contained in jqplot.sprintf.js by Ash Searle:
+ *
+ *     version 2007.04.27
+ *     author Ash Searle
+ *     http://hexmen.com/blog/2007/03/printf-sprintf/
+ *     http://hexmen.com/js/sprintf.js
+ *     The author (Ash Searle) has placed this code in the public domain:
+ *     "This code is unrestricted: you are free to use it however you like."
+ *
+ * jsDate library by Chris Leonello:
+ *
+ * Copyright (c) 2010-2011 Chris Leonello
+ *
+ * jsDate is currently available for use in all personal or commercial projects 
+ * under both the MIT and GPL version 2.0 licenses. This means that you can 
+ * choose the license that best suits your project and use it accordingly.
+ *
+ * jsDate borrows many concepts and ideas from the Date Instance 
+ * Methods by Ken Snyder along with some parts of Ken's actual code.
+ * Ken has generously given permission to adapt his code and release
+ * under the MIT and GPL V2 licenses.
+ * 
+ * Ken's origianl Date Instance Methods and copyright notice:
+ * 
+ * Ken Snyder (ken d snyder at gmail dot com)
+ * 2008-09-10
+ * version 2.0.2 (http://kendsnyder.com/sandbox/date/)     
+ * Creative Commons Attribution License 3.0 (http://creativecommons.org/licenses/by/3.0/)
+ *
+ * jqplotToImage function based on Larry Siden's export-jqplot-to-png.js.
+ * Larry has generously given permission to adapt his code for inclusion
+ * into jqPlot.
+ *
+ * Larry's original code can be found here:
+ *
+ * https://github.com/lsiden/export-jqplot-to-png
+ * 
+ * 
+ */
+
 (function($) {
 
     $.fn.jqplotChildText = function() {
@@ -92,6 +151,26 @@
         newContext.textAlign = 'left';
         newContext.textBaseline = 'top';
 
+        function getLineheight(el) {
+            var lineheight = parseInt($(el).css('line-height'));
+
+            if (isNaN(lineheight)) {
+                lineheight = parseInt($(el).css('font-size')) * 1.2;
+            }
+
+            return lineheight;
+        }
+
+        function writeWrappedText (el, context, text, left, top) {
+            var lineheight = getLineheight(el);
+            var tagwidth = $(el).innerWidth();
+            var tagheight = $(el).innerHeight();
+
+
+
+            
+        }
+
         function _jqpToImage(el, x_offset, y_offset) {
             var tagname = el.tagName.toLowerCase();
             var p = $(el).position();
@@ -113,6 +192,8 @@
                     if ($(el).css('textAlign') === 'center') {
                         left += (newCanvas.width - newContext.measureText(text).width)/2  - transx;
                     }
+
+                    // writeWrappedText(el, newContext, text, left, top);
                     newContext.fillText(text, left, top);
                 }
             }
@@ -175,20 +256,16 @@
         return newCanvas;
     };
 
-    $.fn.jqplotSaveImage = function(fmt) {
+    $.fn.jqplotSaveImage = function() {
         var imgCanvas = $(this).jqplotToImage(0,0);
         if (imgCanvas) {
-            // if (imgCanvas.mozGetAsFile) {
-            //     var f = imgCanvas.mozGetAsFile("test.png");
-            //     console.log(f);
-            // }
             var imgData = imgCanvas.toDataURL("image/png");
             window.location.href = imgData.replace("image/png", "image/octet-stream");
         }
 
     };
 
-    $.fn.jqplotViewImage = function(fmt) {
+    $.fn.jqplotViewImage = function() {
         var imgCanvas = $(this).jqplotToImage(0,0);
         if (imgCanvas) {
             window.open(imgCanvas.toDataURL("image/png"));
