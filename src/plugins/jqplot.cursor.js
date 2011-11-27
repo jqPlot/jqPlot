d4274daf21ba74b909fd9228d9f818607706404e
Chris Leonello <chris@jqplot.com>
2011-11-27 16:27:42 -0500

    Fixed version label.

--- src/plugins/jqplot.cursor.js
+++ src/plugins/jqplot.cursor.js
@@ -2,7 +2,7 @@
  * jqPlot
  * Pure JavaScript plotting plugin using jQuery
  *
- * Version: 1.0.0b2_r1012
+ * Version: @VERSION
  *
  * Copyright (c) 2009-2011 Chris Leonello
  * jqPlot is currently available for use in all personal or commercial projects 
