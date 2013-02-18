d325fe6c81e5b62ae322b3fddf0b6f6ea01f4a5b
Paul Pritchard <ppritcha1@gmail.com>
2013-02-18 10:36:22 -0600

    Fix for #521, invalid JS in meterGaugeRenderer.js

--- src/plugins/jqplot.meterGaugeRenderer.js
+++ src/plugins/jqplot.meterGaugeRenderer.js
@@ -739,7 +739,7 @@
             
             else if (this.label && this.labelPosition == 'bottom') {
                 var l = this._center[0] + this.canvas._offsets.left - this._labelElem.outerWidth(true)/2;
-                var t = this._center[1] + this.canvas._offsets.top + this.innerPad + + this.ringWidth + this.padding + this.labelHeightAdjust;
+                var t = this._center[1] + this.canvas._offsets.top + this.innerPad + this.ringWidth + this.padding + this.labelHeightAdjust;
                 this._labelElem.css({left:l, top:t});
                 
             }
