a399afb180e7f806449d49c05de2115b479866f5
Paul Pritchard <ppritcha1@gmail.com>
2015-11-02 09:45:31 -0600

    Switch from jshint to jslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,14 +193,19 @@ module.exports = function(grunt) {
         }
       },
     },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/jqplot*.js'],
-      options: {
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
+    jslint: {
+      dist: {
+        src: [
+          'src/*.js'
+        ],
+        exclude: [
+          'src/jquery.min.js',
+          'src/jquery.js'
+        ],
+        options: {
+          edition: 'latest',
+          errorsOnly: true,
+          failOnError: false
         }
       }
     },
@@ -263,7 +268,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jslint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
